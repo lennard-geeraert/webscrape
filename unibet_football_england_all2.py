7f4ce89e461d97b004be22a355bc5a26296c179d
Lennard Geeraert <lennard.geeraert@hogent.be>
2022-02-02 21:27:44 +0100

    lezen lukt ongeveer

--- unibet_football_england_all2.py
+++ unibet_football_england_all2.py
@@ -2,100 +2,73 @@ from queue import Empty
 from bs4 import BeautifulSoup
 import requests
 import re
+import time
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import ElementClickInterceptedException
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://nl-sports.unibet.be/betting/sports/filter/football/england/all/matches')
 
-# driver.find_element_by_xpath("//div[@class='e517e']/option[text()='Totaal doelpunten']").click()
+# ----------------------------------    click on live button   --------------------------------------
 
-# search = driver.find_element_by_xpath('.//span[@class = "e517e"]')
+# visible = False
 
-search = driver.find_element_by_css_selector("div._1490e")
+# while not visible:
+#     try:
+#         button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@class = "_80525 _35d20"]')))
+#         button.click()
+#         visible = True
+#     except (ElementClickInterceptedException) as e:
+#         visible = False
 
-print(search.text)
-search.click()
-
-# search2 = driver.find_element_by_css_selector("button._80525")
-
-# search2 = driver.find_element_by_xpath('.//button[@class = "_80525"]')
-
-# search2 = driver.find_element_by_xpath('.//button[contains(text(), "All")]')
-
-# search2 = driver.find_element_by_xpath(".//div[text() = 'All']")
-
-# search2 = driver.find_element_by_class_name("ae466")
-
-# print(search2.text)
-# search2.click()
-
-driver.implicitly_wait(10)
-
-# games = driver.find_elements_by_class_name("_0dfcf") # _4a05b _0dfcf f10e9
-# print(games[0])
-
-# allOdds = []
-# allNames = []
-
-# for game in games:
-#     names = game.find_elements_by_class_name("af24c")
-#     for name in names:
-#         name = name.text.strip()
-#         if(len(name) != 0):
-#             print(name)
-#             allNames.append(name)
-
-#     odds = game.find_elements_by_xpath('.//span[@class = "_5a5c0"]')
-#     for odd in odds:
-#         odd = odd.text.strip()
-#         if(len(odd) != 0):
-#             print(odd)
-#             allOdds.append(odd)
+# ------------------------------------    proberen drop down aan te clicken   --------------------------------------
 
 
-# print(allOdds)
-# print(allNames)
+# search = driver.find_element_by_css_selector("div._1490e")
 
-# allNames = []
+# print(search.text)
+# search.click()
 
-# names = driver.find_elements_by_class_name("af24c")
-# for name in names:
-#         name = name.text.strip()
-#         if(len(name) != 0):
-#             print(name)
-#             allNames.append(name)
-# print(allNames)
+# ---------------------------------------   main data ophalen   ---------------------------------------------
+
+games = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="_0dfcf"]')))
+
+allOdds = []
+allNames = []
+
+for game in games:
+    names = WebDriverWait(driver = game, timeout = 10).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="af24c"]')))
+    for name in names:
+        name = name.text.strip()
+        if(len(name) != 0):
+            print(name)
+            allNames.append(name)
+
+    odds = WebDriverWait(driver = game, timeout = 10).until(EC.presence_of_all_elements_located((By.XPATH, '//span[@class = "_5a5c0"]')))
+    for odd in odds:
+        odd = odd.text.strip()
+        if(len(odd) != 0):
+            print(odd)
+            allOdds.append(odd)
+
+
+print(allOdds)
+print(allNames)
+
+
+time.sleep(5)
+
 
 driver.close()
 driver.quit()
 
+# --------------------   use headers so program don't think you're a bot with beautifulsoup4  -------------------
 
-
-
-
-
-# url = 'https://nl-sports.unibet.be/betting/sports/filter/football/england/all/matches'
 # page_text = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).text
-
-# soup = BeautifulSoup(page_text, 'lxml')
-
-# games = soup.find_all('div', class_ = 'fa117')
-
-# print(games)
-
-# print(soup.find_all("div", string=re.compile("Kameroen")))
-
-
-""" html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
-games = soup.find_all('div')
-print(games) """
-
-
-
-
-
-    
