9980c0aa5e4b74b40766904cbba77ed0b6cbac0b
Lennard Geeraert <lennard.geeraert@hogent.be>
2022-02-02 16:10:24 +0100

    push for pull

--- unibet_football_england_all.py
+++ unibet_football_england_all.py
@@ -10,24 +10,45 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://nl-sports.unibet.be/betting/sports/filter/football/england/all/matches')
 
-games = driver.find_elements_by_class_name("_4a05b")
+# games = driver.find_elements_by_class_name("a9753 _24efa _8f2c1") # _4a05b
+# allOdds = []
+# allNames = []
 
-for game in games:
-    names = game.find_elements_by_class_name("af24c")
-    for name in names:
+# for game in games:
+#     names = game.find_elements_by_class_name("af24c")
+#     for name in names:
+#         name = name.text.strip()
+#         if(len(name) != 0):
+#             print(name)
+#             allNames.append(name)
+
+#     odds = game.find_elements_by_xpath('.//span[@class = "_5a5c0"]')
+#     for odd in odds:
+#         odd = odd.text.strip()
+#         if(len(odd) != 0):
+#             print(odd)
+#             allOdds.append(odd)
+
+
+# print(allOdds)
+# print(allNames)
+
+allNames = []
+
+names = driver.find_elements_by_class_name("af24c")
+for name in names:
         name = name.text.strip()
         if(len(name) != 0):
             print(name)
-
-    odds = game.find_elements_by_xpath('.//span[@class = "_5a5c0"]')
-    for odd in odds:
-        odd = odd.text.strip()
-        if(len(odd) != 0):
-            print(odd)
-
+            allNames.append(name)
+print(allNames)
 
 driver.close()
 driver.quit()
+
+
+
+
 
 
 # url = 'https://nl-sports.unibet.be/betting/sports/filter/football/england/all/matches'
